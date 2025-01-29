2dcd8f6486d34d4503bbf6e730b98b45b43b2d4c
David Valentino <dvlimweijiang@gmail.com>
2025-01-29 22:31:28 +0700

    feat: update main class to support new jasper report mvc

--- src/main/java/com/davidvalentino/manulife_test/ManulifeTestApplication.java
+++ src/main/java/com/davidvalentino/manulife_test/ManulifeTestApplication.java
@@ -1,7 +1,11 @@
 package com.davidvalentino.manulife_test;
 
+import com.davidvalentino.manulife_test.controller.ReportController;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ManulifeTestApplication {
@@ -10,4 +14,11 @@ public class ManulifeTestApplication {
 		SpringApplication.run(ManulifeTestApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner run(ApplicationContext ctx) {
+		return args -> {
+			ReportController reportController = ctx.getBean(ReportController.class);
+			reportController.generateReport();
+		};
+	}
 }
