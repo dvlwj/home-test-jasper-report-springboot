dfd03364497cd3635cec50d80a9e7af98528e5ae
David Valentino <dvlimweijiang@gmail.com>
2025-01-29 21:18:38 +0700

    chore: init springboot project

---
+++ src/main/java/com/davidvalentino/manulife_test/ManulifeTestApplication.java
@@ -0,0 +1,13 @@
+package com.davidvalentino.manulife_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManulifeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManulifeTestApplication.class, args);
+	}
+
+}
